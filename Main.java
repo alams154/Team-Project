6dcf6281b142a493f8ac6e1890b7d821129a7c4a
bradzom <31576631+bradzom@users.noreply.github.com>
2019-04-23 20:56:25 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,21 +1,34 @@
 package application;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.Hashtable;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.scene.text.TextFlow;
 
 
@@ -26,8 +39,10 @@ public class Main extends Application {
    * @param filepath
    * @throws IOException
    * @throws ParseException
+   * @throws org.json.simple.parser.ParseException
    */
-  public static void parseJSON(String filepath) throws IOException, ParseException {
+  public static void parseJSON(String filepath)
+      throws IOException, ParseException, org.json.simple.parser.ParseException {
     // astroexample1
     FileReader f = new FileReader(filepath);
     // first, parse JSON file given the file path
@@ -114,23 +129,45 @@ public class Main extends Application {
   @Override
   public void start(Stage primaryStage) {
     try {
-
       BorderPane mainMenu = new BorderPane();
-      Button powerButton = new Button();
+
+      // Layout Setup
+      Image bg = new Image("file:hs-1996-01-a-large_web.jpg");
+      mainMenu.setBackground(new Background(new BackgroundImage(bg, BackgroundRepeat.NO_REPEAT,
+          BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(
+              BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false))));
+      Image power = new Image("file:Power.png");
+      ImageView powerView = new ImageView(power);
+      powerView.setFitHeight(40);
+      powerView.setFitWidth(40);
+      Button powerButton = new Button(null, powerView);
       mainMenu.setRight(powerButton);
       Text title = new Text("Astronomical Catalog Data Receiver");
+      title.setFont(Font.font(40));
+      title.setFill(Color.WHITE);
       TextFlow titleBox = new TextFlow(title);
-
+      titleBox.setTextAlignment(TextAlignment.CENTER);
       mainMenu.setTop(titleBox);
-      TextArea filePath = new TextArea();
-      mainMenu.setCenter(filePath);
-      Button readFile = new Button();
+      Button fileChooser = new Button("Select File");
+      fileChooser.setFont(Font.font(20));
+      mainMenu.setCenter(fileChooser);
+      Button recentFile = new Button("Read most recent file");
+      recentFile.setFont(Font.font(20));
+      Button recentID = new Button("Read all data from most recent ID");
+      recentID.setFont(Font.font(20));
+      VBox bottomRight = new VBox(recentFile, recentID);
+      bottomRight.setAlignment(Pos.BOTTOM_RIGHT);
+      mainMenu.setBottom(bottomRight);
 
-      Scene scene1 = new Scene(mainMenu, 1280, 720);
+      Scene scene1 = new Scene(mainMenu, 780, 800);
       scene1.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
 
       BorderPane input = new BorderPane();
+
+      // Layout Setup
+
+
       Scene scene2 = new Scene(input, 1280, 720);
       scene2.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
@@ -142,6 +179,23 @@ public class Main extends Application {
       Scene scene4 = new Scene(exit, 1280, 720);
       scene4.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
+      // Action Setup
+      FileChooser chooser = new FileChooser();
+      fileChooser.setOnAction(e -> {
+        File selectedFile = chooser.showOpenDialog(primaryStage);
+      });
+      recentFile.setOnAction(e -> {
+        primaryStage.setScene(scene3);
+      });
+      recentID.setOnAction(e -> {
+        primaryStage.setScene(scene3);
+      });
+      powerButton.setOnAction(e -> {
+        primaryStage.setScene(scene4);
+      });
+
+      // Action Setup
+
       primaryStage.setScene(scene1);
       primaryStage.show();
 
@@ -152,7 +206,8 @@ public class Main extends Application {
     }
   }
 
-  public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+  public static void main(String[] args) throws FileNotFoundException, IOException, ParseException,
+      org.json.simple.parser.ParseException {
     h = new Hashtable<Integer, SourceObject>();
     String filepath = "astroexample1.json";
     parseJSON(filepath);
