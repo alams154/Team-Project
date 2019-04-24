e767f2335685d20381ab2f92748a681190d445ee
jhoeg1 <47787245+jhoeg1@users.noreply.github.com>
2019-04-23 23:00:20 -0500

    Add files via upload
    changed inputRightPane() and checkall()

--- Main.java
+++ Main.java
@@ -1,7 +1,4 @@
-package application;
-
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -18,7 +15,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -32,7 +31,6 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
@@ -41,11 +39,94 @@ import javafx.scene.text.TextFlow;
 
 public class Main extends Application {
   private static Hashtable<Integer, SourceObject> h;
-  private static Scene scene1;
-  private static Scene scene2;
-  private static Scene scene3;
-  private static Scene scene4;
+
   
+  private Scene screen1Setup(Stage primaryStage, Scene scene1, Scene scene2, Scene scene3, Scene scene4) {
+		BorderPane mainMenu = new BorderPane();
+		
+		Image bg = new Image("file:hs-1996-01-a-large_web.jpg");
+		mainMenu.setBackground(new Background(new BackgroundImage(bg, BackgroundRepeat.NO_REPEAT,
+				BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+				new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false))));
+		Image power = new Image("file:Power.png");
+		ImageView powerView = new ImageView(power);
+		powerView.setFitHeight(40);
+		powerView.setFitWidth(40);
+		Button powerButton = new Button(null, powerView);
+		mainMenu.setRight(powerButton);
+		Text title = new Text("Astronomical Catalog Data Receiver");
+		title.setFont(Font.font(40));
+		title.setFill(Color.WHITE);
+		TextFlow titleBox = new TextFlow(title);
+		titleBox.setTextAlignment(TextAlignment.CENTER);
+		mainMenu.setTop(titleBox);
+		Button fileChooser = new Button("Select File");
+		fileChooser.setFont(Font.font(20));
+		mainMenu.setCenter(fileChooser);
+		Button recentFile = new Button("Read most recent file");
+		recentFile.setFont(Font.font(20));
+		Button recentID = new Button("Read all data from most recent ID");
+		recentID.setFont(Font.font(20));
+		VBox bottomRight = new VBox(recentFile, recentID);
+		bottomRight.setAlignment(Pos.BOTTOM_RIGHT);
+		mainMenu.setBottom(bottomRight);
+		
+		FileChooser chooser = new FileChooser();
+		fileChooser.setOnAction(e -> {
+			File selectedFile = chooser.showOpenDialog(primaryStage);
+			if(selectedFile.getName().substring(selectedFile.getName().length() - 5, selectedFile.getName().length()).equals(".json")) {
+				primaryStage.setScene(scene2);
+			}
+			else {
+				Alert invalidFile = new Alert (Alert.AlertType.INFORMATION, "Incorrect File Type, Choose a .json file");
+				invalidFile.showAndWait().filter(response -> response == ButtonType.OK);
+			}
+		});
+		recentFile.setOnAction(e -> {
+			primaryStage.setScene(scene3);
+		});
+		recentID.setOnAction(e -> {
+			primaryStage.setScene(scene3);
+		});
+		powerButton.setOnAction(e -> {
+			primaryStage.setScene(scene4);
+		});
+		
+		return new Scene(mainMenu, 780, 800);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Scene scene1 = new Scene(new BorderPane());
+			
+			// Layout Setup
+			BorderPane input = new BorderPane();
+			Scene scene2 = new Scene(input, 1280, 720);
+			scene2.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+			BorderPane results = new BorderPane();
+			Scene scene3 = new Scene(results, 1280, 720);
+			scene3.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+			BorderPane exit = new BorderPane();
+			Scene scene4 = new Scene(exit, 1280, 720);
+			scene4.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			scene1 = screen1Setup(primaryStage, scene1, scene2, scene3, scene4);
+			// Layout Setup
+
+			scene1.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+
+
+			primaryStage.setScene(scene1);
+			primaryStage.show();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
   
   /**
    * @param filepath
@@ -53,7 +134,7 @@ public class Main extends Application {
    * @throws ParseException
    * @throws org.json.simple.parser.ParseException
    */
-  private static void parseJSON(String filepath)
+  public static void parseJSON(String filepath)
       throws IOException, ParseException, org.json.simple.parser.ParseException {
     // astroexample1
     FileReader f = new FileReader(filepath);
@@ -138,131 +219,107 @@ public class Main extends Application {
     }
   }
 
-	private Scene screen1Setup(Stage primaryStage) {
-		BorderPane mainMenu = new BorderPane();
-		
-		Image bg = new Image("file:hs-1996-01-a-large_web.jpg");
-		mainMenu.setBackground(new Background(new BackgroundImage(bg, BackgroundRepeat.REPEAT,
-				BackgroundRepeat.REPEAT, BackgroundPosition.CENTER,
-				new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false))));
-		Image power = new Image("file:Power.png");
-		ImageView powerView = new ImageView(power);
-		powerView.setFitHeight(40);
-		powerView.setFitWidth(40);
-		Button powerButton = new Button(null, powerView);
-		mainMenu.setRight(powerButton);
-		Text title = new Text("Astronomical Catalog Data Receiver");
-		title.setFont(Font.font(40));
-		title.setFill(Color.WHITE);
-		TextFlow titleBox = new TextFlow(title);
-		titleBox.setTextAlignment(TextAlignment.CENTER);
-		mainMenu.setTop(titleBox);
-		Button fileChooser = new Button("Select File");
-		fileChooser.setFont(Font.font(20));
-		mainMenu.setCenter(fileChooser);
-		Button recentFile = new Button("Read most recent file");
-		recentFile.setFont(Font.font(20));
-		Button recentID = new Button("Read all data from most recent ID");
-		recentID.setFont(Font.font(20));
-		VBox bottomRight = new VBox(recentFile, recentID);
-		bottomRight.setAlignment(Pos.BOTTOM_RIGHT);
-		mainMenu.setBottom(bottomRight);
-		
-		
-		FileChooser chooser = new FileChooser();
-		fileChooser.setOnAction(e -> {
-			File selectedFile = chooser.showOpenDialog(primaryStage);
-			if(selectedFile.getName().substring(selectedFile.getName().length() - 5, selectedFile.getName().length()).equals(".json")) {
-				primaryStage.setScene(scene2);
-			}
-			else {
-				Alert invalidFile = new Alert (Alert.AlertType.INFORMATION, "Incorrect File Type, Choose a .json file");
-				invalidFile.showAndWait().filter(response -> response == ButtonType.OK);
-			}
-		});
-		recentFile.setOnAction(e -> {
-			primaryStage.setScene(scene3);
-		});
-		recentID.setOnAction(e -> {
-			primaryStage.setScene(scene3);
-		});
-		powerButton.setOnAction(e -> {
-			primaryStage.setScene(scene4);
-		});
-		
-		return new Scene(mainMenu, 1200, 800);
-	}
-	
-//	private Scene screen3Setup(Stage primaryStage, Scene scene1, Scene scene2, Scene scene3, Scene scene4) {
-//		
-//	}
-	
-	private Scene screen4Setup() {
-		BorderPane pane = new BorderPane();
-		Text text1 = new Text("Thank you for using \n      our program!");
-	    text1.setFill(Color.WHITE);
-	    text1.setFont(Font.font("Helvetica", FontWeight.BOLD, 40));
-	    Text text2 = new Text("Now exiting...");
-	    text2.setFill(Color.WHITE);
-	    text2.setFont(Font.font("Helvetica", FontPosture.ITALIC, 20));
-	     
-	    VBox bottomRight = new VBox(text2);
-	    bottomRight.setAlignment(Pos.BOTTOM_RIGHT);
-	    
-	    pane.setCenter(text1);
-	    pane.setPrefSize(500, 300);
-	    pane.setBottom(bottomRight);		   
-	     
-	    Image image = new Image("file:hs-1996-01-a-large_web.jpg"); 
-	    
-	    Scene exitScene = new Scene(pane, 1200, 800);
-	    exitScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-	    BackgroundImage back = new BackgroundImage(image, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
-	    pane.setBackground(new Background(back));
-	    return exitScene;
-	}
-
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			scene1 = screen1Setup(primaryStage);
-			//scene2 = ;
-			//scene3 = ;
-			scene4 = screen4Setup();
-			// Layout Setup
-
-			scene1.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			//scene2.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			//scene3.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			scene4.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-
-
-
-			primaryStage.setScene(scene1);
-			primaryStage.show();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
   
-  private void idParameters(BorderPane pane) {
-	  
-  }
-  
-  private void idLookUp(BorderPane pane) {
-	    TextField enterID = new TextField();
+  private void inputRightPane(BorderPane pane) {
+	  	TextField enterID = new TextField();
 	    enterID.setPromptText("Enter ID");
-	    Label label1 = new Label("ID: ");
+	    
+	    Label label1 = new Label("Search ID: ");
 	    label1.setTextFill(Color.WHITE);
 	    label1.setFont(Font.font("Helvetica", FontWeight.BOLD, 24));
+	    
 	    HBox hb = new HBox(label1, enterID);
-	    hb.setAlignment(Pos.TOP_RIGHT);
-	    pane.setRight(hb);
-}
+	    hb.setAlignment(Pos.BOTTOM_RIGHT);
 
-  public static void main(String[] args) throws FileNotFoundException, IOException, ParseException,
-      org.json.simple.parser.ParseException {
+	    
+		CheckBox ra = new CheckBox("Right Ascension");
+		ra.setTextFill(Color.WHITE);
+		ra.setFont(Font.font("Helvetica", 24));
+		CheckBox dec = new CheckBox("Declination");
+		dec.setTextFill(Color.WHITE);
+		dec.setFont(Font.font("Helvetica", 24));
+	    CheckBox hflux = new CheckBox("Hard-Band Flux");
+	    hflux.setTextFill(Color.WHITE);
+	    hflux.setFont(Font.font("Helvetica", 24));
+	    CheckBox sflux = new CheckBox("Soft-Band Flux");
+	    sflux.setTextFill(Color.WHITE);
+	    sflux.setFont(Font.font("Helvetica", 24));
+	    CheckBox z = new CheckBox("Redshift");
+	    z.setTextFill(Color.WHITE);
+	    z.setFont(Font.font("Helvetica", 24));
+	    CheckBox rmag = new CheckBox("R-Band Magnitude");
+	    rmag.setTextFill(Color.WHITE);
+	    rmag.setFont(Font.font("Helvetica", 24));
+	    
+	    VBox first = new VBox(ra, dec, hflux);
+	    first.setSpacing(100);
+	    VBox sec = new VBox(sflux, z , rmag);
+	    sec.setSpacing(100);
+	    HBox boxes = new HBox(first, sec);
+	    boxes.setSpacing(30);
+	    
+	    VBox moreboxes = new VBox(hb, boxes);
+	    moreboxes.setSpacing(150);
+	    moreboxes.setTranslateX(-20);
+	    moreboxes.setTranslateY(10);
+	    pane.setRight(moreboxes);
+	    
+	    //bottom buttons
+	    Button addSource = new Button("Add Source");
+	    addSource.setFont(Font.font("Helvetica", 18));
+	    Button display = new Button("Display");
+	    display.setFont(Font.font("Helvetica", 18));
+	    Button write = new Button("Write");
+	    write.setFont(Font.font("Helvetica", 18));
+	    HBox botButtons = new HBox(addSource, display, write);
+	    botButtons.setSpacing(100);
+	    botButtons.setAlignment(Pos.CENTER);
+	    botButtons.setTranslateY(-30);
+	    pane.setBottom(botButtons);
+}
+  private void checkall(CheckBox ra, CheckBox dec, CheckBox hflux, CheckBox sflux, CheckBox z, CheckBox rmag, CheckBox all) {
+		if(all.isSelected()) {
+			if(!ra.isSelected()) {
+				ra.fire();
+			}
+			if(!dec.isSelected()) {
+				dec.fire();
+			}
+			if(!hflux.isSelected()) {
+				hflux.fire();
+			}
+			if(!sflux.isSelected()) {
+				sflux.fire();
+			}
+			if(!z.isSelected()) {
+				z.fire();
+			}
+			if(!rmag.isSelected()) {
+				rmag.fire();
+			}
+		} else {
+			if(ra.isSelected()) {
+				ra.fire();
+			}
+			if(dec.isSelected()) {
+				dec.fire();
+			}
+			if(hflux.isSelected()) {
+				hflux.fire();
+			}
+			if(sflux.isSelected()) {
+				sflux.fire();
+			}
+			if(z.isSelected()) {
+				z.fire();
+			}
+			if(rmag.isSelected()) {
+				rmag.fire();
+			}
+		}
+	}
+
+  public static void main(String[] args) throws FileNotFoundException, IOException, ParseException, Exception {
     h = new Hashtable<Integer, SourceObject>();
     String filepath = "astroexample1.json";
     parseJSON(filepath);
