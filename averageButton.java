b34c507728aa3d570a42d07eb91ec4161a048b72
Sameer Alam <salam4@users.noreply.github.com>
2019-05-03 06:48:03 -0500

    Methodized some methods but did not finish commenting

--- averageButton.java
+++
@@ -1,5 +0,0 @@
-Button average = new Button("Compute mean distance");
-average.setOnAction(e -> {
-    Alert meanDistance = new Alert(Alert.AlertType.INFORMATION, "Mean Distance: " + averageDistance);
-    meanDistance.showAndWait().filter(response -> response == ButtonType.OK);
-});
